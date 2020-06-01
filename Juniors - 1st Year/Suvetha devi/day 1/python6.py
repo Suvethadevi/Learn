1e288ef2588f457eca24ef4d10cba0907b7e93ae
Suvethadevi <naveenragunathan1830@gmail.com>
2020-05-31 23:41:24 -0700

    Day 1 python tasks

---
+++ Juniors - 1st Year/Suvetha devi/day 1/python6.py
@@ -0,0 +1,9 @@
+x=input("enter the dividend to operate")
+x=int(x)
+y=input("\n enter the  divisor to operate")
+y=int(y)
+print("\n the quotient is")
+print(x/y)
+print ("\n the remainder is")
+print(x%y)
+
